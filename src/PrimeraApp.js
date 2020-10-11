4f36311009e81f359506edb31d9ab59f1e5abf60
caemci <carlosecimino@gmail.com>
2020-10-11 17:32:56 -0300

    Fragments

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const PrimeraApp = () => {
-	return <h1>Hola mundo</h1>;
+	return (
+		<>
+			<h1>Hola mundo</h1>
+			<p>Mi primera app</p>
+		</>
+	);
+
+	/* Dejar las etiquetas vacías es similar a:
+		<Fragment>
+			<h1>Hola mundo</h1>
+			<p>Mi primera app</p>
+		<Fragment/>
+	*/
 }
 
 export default PrimeraApp;
