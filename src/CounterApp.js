a7fb9a04a7615c3f7fe3caaefe557614e02d18d0
caemci <carlosecimino@gmail.com>
2020-10-11 18:18:57 -0300

    Hooks - useState

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,13 +1,25 @@
 // Snippets: rafc, rafce, rafcp
 
-import React from 'react'; // Snippet: imr
+import React, { useState } from 'react'; // Snippet: imr
 import PropTypes from 'prop-types';
 
 const CounterApp = ({ value }) => {
+
+	const [counter, setCounter] = useState(0);
+
+	//handleAdd 
+	const handleAdd = e => {
+		setCounter(counter + 1);
+		//setCounter((c) => c + 1); // Otra forma
+	}
+
+
 	return (
 		<>
 			<h1>CounterApp</h1>
-			<h2>{value}</h2>
+			<h2>{counter}</h2>
+
+			<button onClick={handleAdd}>+1</button>
 		</>
 	);
 }
